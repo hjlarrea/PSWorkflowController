8b80064d1b63b89945a176067cdde8c0a0106ba1
Hernan J. Larrea <hjlarrea@hotmail.com>
2018-07-25 13:31:04 -0300

    More corrections.

--- Invoke-PSWorkflowController.ps1
+++ Invoke-PSWorkflowController.ps1
@@ -3,7 +3,7 @@
 param (
 
     [parameter (mandatory=$true,parameterSetName="Start")][Switch]$Start,
-    [parameter (mandatory=$true,parameterSetName="Start")][String]$WorkFlowScript,
+    [parameter (mandatory=$true,parameterSetName="Start")][String]$WorkflowScript,
     
     [parameter (mandatory=$true,parameterSetName="ScheduleResume")][Switch]$ScheduleResume,
 
@@ -17,17 +17,17 @@ param (
     [parameter (mandatory=$true,parameterSetName="ScheduleResume")]
     [parameter (mandatory=$true,parameterSetName="Resume")]
     [parameter (mandatory=$true,parameterSetName="ScheduleCleanup")]
-    [parameter (mandatory=$true,parameterSetName="CleanUp")][String]$WorkFlow
+    [parameter (mandatory=$true,parameterSetName="CleanUp")][String]$Workflow
 )
 
 switch($True) {
 
     "$Start" {
-        #Start WorkFlow
+        #Start Workflow
         #This acticity is to be consumed by the entity starting the workflow, in most of the cases this would be a user script
 
         #Create scheduled task
-        $stAction=New-ScheduledTaskAction -Execute "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -Argument "-File $WorkFlowScript"
+        $stAction=New-ScheduledTaskAction -Execute "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -Argument "-File $WorkflowScript"
         $stSettings=New-ScheduledTaskSettingsSet
         Register-ScheduledTask -Action $stAction -TaskName "Start_$WorkFLow" -User System -RunLevel Highest -Settings $stSettings -ErrorAction SilentlyContinue | Out-Null
         
@@ -36,11 +36,11 @@ switch($True) {
     }
 
     "$ScheduleResume" {
-        #Schedule WorkFlow Resume
+        #Schedule Workflow Resume
         #This activity is to be consume by the script implementing the workflow
 
         #Create scheduled task
-        $stAction=New-ScheduledTaskAction -Execute "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -Argument "-File $PSScriptRoot\Invoke-WorkflowController.ps1 -Resume -WorkFlow $WorkFlow"
+        $stAction=New-ScheduledTaskAction -Execute "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -Argument "-File $PSScriptRoot\Invoke-WorkflowController.ps1 -Resume -Workflow $Workflow"
         $stSettings=New-ScheduledTaskSettingsSet
         $stTrigger=New-ScheduledTaskTrigger -AtStartup -RandomDelay 00:05:00
         Register-ScheduledTask -Action $stAction -TaskName "Resume_$WorkFLow" -User System -RunLevel Highest -Settings $stSettings -Trigger $stTrigger -ErrorAction SilentlyContinue | Out-Null
@@ -48,7 +48,7 @@ switch($True) {
 
     "$Resume" {
         #Resume workflow jobs
-        Get-Job -State Suspended | Where-Object -FilterScript { ($_.PSJobTypeName -eq "PSWorkflowJob") -and ( $_.Command -eq $WorkFlow) } | Resume-Job
+        Get-Job -State Suspended | Where-Object -FilterScript { ($_.PSJobTypeName -eq "PSWorkflowJob") -and ( $_.Command -eq $Workflow) } | Resume-Job
         Start-Sleep -Seconds 14400 #Sleeps for 4 hours (idle time for the workflow to complete)
     }
 
@@ -57,7 +57,7 @@ switch($True) {
         #This activity is to be consume by the script implementing the workflow
         
         #Create scheduled task
-        $stAction=New-ScheduledTaskAction -Execute "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -Argument "-File $PSScriptRoot\Invoke-WorkflowController.ps1 -Cleanup -WorkFlow $WorkFlow"
+        $stAction=New-ScheduledTaskAction -Execute "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -Argument "-File $PSScriptRoot\Invoke-WorkflowController.ps1 -Cleanup -Workflow $Workflow"
         $stSettings=New-ScheduledTaskSettingsSet
         Register-ScheduledTask -Action $stAction -TaskName "Cleanup_$WorkFLow" -User System -RunLevel Highest -Settings $stSettings -ErrorAction SilentlyContinue | Out-Null
         
@@ -67,8 +67,8 @@ switch($True) {
 
     "$CleanUp" {
         #Cleanup workflow jobs
-        Get-Job | Where-Object -FilterScript { ($_.PSJobTypeName -eq "PSWorkflowJob") -and ( $_.Command -eq $WorkFlow) } | Stop-Job
-        Get-Job | Where-Object -FilterScript { ($_.PSJobTypeName -eq "PSWorkflowJob") -and ( $_.Command -eq $WorkFlow) } | Remove-Job
+        Get-Job | Where-Object -FilterScript { ($_.PSJobTypeName -eq "PSWorkflowJob") -and ( $_.Command -eq $Workflow) } | Stop-Job
+        Get-Job | Where-Object -FilterScript { ($_.PSJobTypeName -eq "PSWorkflowJob") -and ( $_.Command -eq $Workflow) } | Remove-Job
         Unregister-ScheduledTask -TaskName "Start_$Workflow" -Confirm:$false
         Unregister-ScheduledTask -TaskName "Resume_$Workflow" -Confirm:$false
         Unregister-ScheduledTask -TaskName "Cleanup_$Workflow" -Confirm:$false
